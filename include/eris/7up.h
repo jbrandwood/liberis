7ad5a27a6d464d0cf4ac70451a20d22632195ba9
Alex Marshall <trap15@raidenii.net>
2011-03-27 23:12:50 -0700

    Added a few new commands.

--- include/eris/7up.h
+++ include/eris/7up.h
@@ -1,6 +1,15 @@
 #ifndef _LIBERIS_7UP_H_
 #define _LIBERIS_7UP_H_
 
+typedef enum {
+	ERIS_7UP_MAP_32X32 = 0,
+	ERIS_7UP_MAP_64X32 = 1,
+	ERIS_7UP_MAP_128X32 = 2,
+	ERIS_7UP_MAP_32X64 = 4,
+	ERIS_7UP_MAP_64X64 = 5,
+	ERIS_7UP_MAP_128X64 = 7
+} eris_7up_mapsize;
+
 void eris_7up_set_vram_write_addr(int chip, u16 addr);
 void eris_7up_vram_write(int chip, u16 data);
 void eris_7up_set_vram_read_addr(int chip, u16 addr);
@@ -12,6 +21,7 @@ void eris_7up_set_scroll(int chip, u16 x, u16 y);
 void eris_7up_do_dma(int chip, u16 src, u16 dst, u16 len);
 void eris_7up_setup_dma(int chip, int dsr_dma, int src_dec, int dst_dec, int vram_irq, int satb_irq);
 void eris_7up_set_satb_address(int chip, u16 addr);
+void eris_7up_set_access_width(int chip, int color_mode, eris_7up_mapsize mapsize, int spr_px_w, int vram_px_w);
 
 #endif
 
