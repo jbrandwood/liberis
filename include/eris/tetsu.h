7045b674a389780f18186f377637cd04b192f3fe
Alex Marshall <trap15@raidenii.net>
2011-04-03 23:15:14 -0700

    Added cellophane support to tetsu.h

--- include/eris/tetsu.h
+++ include/eris/tetsu.h
@@ -29,6 +29,13 @@ typedef enum {
 	TETSU_COLORS_256 = 1,
 } tetsu_colordepth;
 
+typedef enum {
+	TETSU_NO_COEFF = 0,
+	TETSU_COEFF_1 = 1,
+	TETSU_COEFF_2 = 2,
+	TETSU_COEFF_3 = 3,
+} tetsu_coeff;
+
 void eris_tetsu_init();
 void eris_tetsu_set_video_mode(tetsu_lines lines, int ext_sync, tetsu_dotclock dotclock, tetsu_colordepth bg_depth,
 				tetsu_colordepth spr_depth, int bg7up_show, int spr7up_show,
@@ -39,6 +46,10 @@ void eris_tetsu_set_king_palette(u8 bg0, u8 bg1, u8 bg2, u8 bg3);
 void eris_tetsu_set_rainbow_palette(u8 rainbow);
 void eris_tetsu_set_priorities(u8 bg, u8 spr, u8 rainbow, u8 bg0, u8 bg1, u8 bg2, u8 bg3);
 void eris_tetsu_set_chroma_key(u8 maxy, u8 miny, u8 maxu, u8 minu, u8 maxv, u8 minv);
+void eris_tetsu_set_cellophane_color(u16 color);
+void eris_tetsu_set_cellophane_sprites(int* banks); /* 16 element array */
+void eris_tetsu_set_cellophane_control(int enable, int in_front, tetsu_coeff bg, tetsu_coeff spr, tetsu_coeff rainbow, tetsu_coeff bg0, tetsu_coeff bg1, tetsu_coeff bg2, tetsu_coeff bg3);
+void eris_tetsu_set_cellophane_coeffs(int coeff, u8 ay, u8 au, u8 av, u8 by, u8 bu, u8 bv);
 
 #endif
 
